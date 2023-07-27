6b7abef8547ce1365abd8714d95f2d803557a238
sofea <sofeanick59@gmail.com>
2023-07-27 11:36:23 +0400

    Initial Commit

---
+++ Todo_unit8app/Todo_unit8appApp.swift
@@ -0,0 +1,17 @@
+//
+//  Todo_unit8appApp.swift
+//  Todo_unit8app
+//
+//  Created by sofea hakim on 27/07/2023.
+//
+
+import SwiftUI
+
+@main
+struct Todo_unit8appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
